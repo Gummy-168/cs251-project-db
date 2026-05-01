535d76b7b516b89e60c9c36a0bcda6edf1ba436c
Karin Supareang <karin.sup@dome.tu.ac.th>
2026-05-01 21:49:13 +0700

    fix: add __init__.py so module can be imported properly

---
+++ Backend/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .movie import MovieCreate, MovieUpdate, MovieResponse
+from .showTime import ShowtimeCreate, ShowtimeResponse
+from .branch import BranchCreate, BranchResponse
+from .theater import TheaterCreate, TheaterResponse
+from .seat import SeatCreate, SeatResponse
+from .booking import BookingCreate, BookingResponse
+from .payment import PaymentCreate, PaymentResponse
+from .review import ReviewCreate, ReviewResponse
+from .ticket import TicketCreate, TicketResponse
