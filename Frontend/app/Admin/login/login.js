b04cdfd43f006feec1ed671135cc97b451765e5c
MarineNumber1 <notesun55@gmail.com>
2026-05-02 01:21:19 +0700

    Admin login html, css, js

---
+++ Frontend/app/Admin/login/login.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // ตัวอย่าง Log ดูค่าที่รับมา
+    console.log("Signing in with:", email);
+
+    if(email && password) {
+        alert('ยินดีต้อนรับสู่ Emerald Cinema!');
+    } else {
+        alert('กรุณากรอกข้อมูลให้ครบถ้วน');
+    }
+});
